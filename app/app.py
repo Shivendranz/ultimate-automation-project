ec78d8274a850705203b952fe18ca46d3bc9cf82
Shivendranz <bhiv550@yahoo.com>
2026-04-09 15:54:09 +0530

    Added Postgres and MongoDB monitoring

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, jsonify
+from prometheus_flask_exporter import PrometheusMetrics  # <--- Monitoring library
 import redis
 import psutil
 import os
 
 app = Flask(__name__)
 
+# Prometheus Metrics ko initialize kar rahe hain
+# Ye apne aap '/metrics' endpoint bana dega
+metrics = PrometheusMetrics(app)
+
+# static information metrics ke liye (Optional)
+metrics.info('app_info', 'Application info', version='1.0.0')
+
+# Redis setup
 r = redis.Redis(host=os.getenv('REDIS_HOST', 'localhost'), port=6379, decode_responses=True)
 
 @app.route('/')
@@ -19,6 +28,7 @@ def stats():
         hits = r.incr('visitor_count')
     except:
         hits = "Redis Offline"
+    
     return jsonify({
         "cpu": f"{cpu}%",
         "ram": f"{ram}%",
@@ -27,4 +37,5 @@ def stats():
     })
 
 if __name__ == '__main__':
+    # Port 5000 par run ho raha hai
     app.run(host='0.0.0.0', port=5000)
